1e7ec6f331079860c4487829b19f1be1c60e471a
midas06 <harrison_lim@hotmail.com>
2016-05-27 17:22:50 +1200

    initial commit

---
+++ app/src/main/java/com/example/harri/snapshottesting/LoginActivity2.java
@@ -0,0 +1,128 @@
+package com.example.harri.snapshottesting;
+
+import android.app.ProgressDialog;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class LoginActivity2 extends AppCompatActivity {
+
+    private static final String TAG="LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @InjectView(R.id.inputEmail) EditText emailText;
+    @InjectView(R.id.inputPassword)EditText passwordText;
+    @InjectView(R.id.btnLogin) Button loginButton;
+    @InjectView(R.id.linkSignup) TextView signupLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+        ButterKnife.inject(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        signupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        loginButton.setEnabled(false);
+
+        final ProgressDialog loginProgressDialog = new ProgressDialog(LoginActivity2.this, R.style.AppTheme_Dark_Dialog);
+//        final ProgressDialog loginProgressDialog = new ProgressDialog(LoginActivity2.this);
+        loginProgressDialog.setIndeterminate(true);
+        loginProgressDialog.setMessage("Authenticating...");
+        loginProgressDialog.show();
+
+        // TODO: integrate auth logic
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // either call success or fail
+                        onLoginSuccess();
+                        loginProgressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+                // TODO: signup logic
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // overrides using back button to go to the main activity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        loginButton.setEnabled(true);
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean isValid = true;
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailText.setError("enter a valid email address");
+            isValid = false;
+        } else {
+            emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 8) {
+            passwordText.setError("at least 8 characters");
+            isValid = false;
+        } else {
+            passwordText.setError(null);
+        }
+
+        return isValid;
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/harri/snapshottesting/SignupActivity.java
@@ -0,0 +1,124 @@
+package com.example.harri.snapshottesting;
+
+import android.app.ProgressDialog;
+
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignupActivity";
+    @InjectView(R.id.inputName) EditText nameText;
+    @InjectView(R.id.inputEmail) EditText emailText;
+    @InjectView(R.id.inputPassword) EditText passwordText;
+    @InjectView(R.id.btnSignup) Button signupButton;
+    @InjectView(R.id.linkLogin) TextView loginLink;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.inject(this);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this, R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating account");
+        progressDialog.show();
+
+        String name = nameText.getText().toString();
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        // TODO: signup logic here
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        //onSignupSuccess();
+
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    public void onSignupSuccess() {
+        signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean isValid = true;
+
+        String name = nameText.getText().toString();
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+            nameText.setError("at least 3 characters");
+            isValid = false;
+        } else {
+            nameText.setError(null);
+        }
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailText.setError("enter a valid email address");
+            isValid = false;
+        } else {
+            emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 8) {
+            passwordText.setError("at least 8 characters");
+            isValid = false;
+        } else {
+            passwordText.setError(null);
+        }
+
+        return isValid;
+    }
+
+
+}
